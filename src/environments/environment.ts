b7df2f1e18884f1f3311ef8e04ece263eee0a823
freddyastudillo55 <freddy.astudillo.sup@gmail.com>
2026-05-30 16:16:31 -0500

    fix: configure environment variables for Vercel deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080'
+  production: true,
+  apiUrl: typeof process !== 'undefined' && process.env?.['API_URL']
+    ? process.env['API_URL']
+    : 'http://localhost:8080'
 };
